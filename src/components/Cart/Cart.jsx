e5bdb5252cbbc856835d6309aefe868d6ceeffce
iibrahim70 <aviknuhash70@gmail.com>
2023-04-05 10:49:45 +0600

    added react router

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,17 +4,19 @@ import './Cart.css'
 const Cart = ({cart}) => {
   let totalPrice = 0; 
   let totalShipping = 0; 
-
+  let quantity = 0;
   for(const product of cart){
-    totalPrice += product.price;
+    product.quantity = product.quantity || 1;
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping += product.shipping;
+    quantity += product.quantity;
   }
   const tax = totalPrice*7/100; 
   const grandTotal = totalPrice + totalShipping + tax; 
   return (
     <div className='cart'>
       <h4>order summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Shipping: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
---
+++ src/components/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <div>
+      <h1 className='text-orange-500'>helo</h1>
+    </div>
+  );
+};
+
+export default Orders;
